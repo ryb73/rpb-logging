66180e29c9ab65500210ff3b3f22f7f32ff49077
Ryan Biwer <rpbiwer@gmail.com>
2015-06-23 16:25:49 -0500

    generalize

--- index.js
+++ index.js
@@ -5,20 +5,30 @@ var bunyan   = require("bunyan"),
 
 var DATE_FORMAT = "%Y-%m-%d-%H-%M-%S";
 
-module.exports = bunyan.createLogger({
-  name: "index",
-  streams: [
-    {
-      level: "trace",
-      stream: process.stdout
-    },
-    {
-      level: "debug",
-      path: "log/" + strftime(DATE_FORMAT) + ".log"
-    },
-    {
-      level: "error",
-      path: "log/error/" + strftime(DATE_FORMAT) + ".log"
-    }
-  ]
-});
+var loggers = {};
+
+module.exports = function createLogger(name) {
+  name = name || "index";
+
+  if(!loggers[name]) {
+    loggers[name] = bunyan.createLogger({
+      name: name,
+      streams: [
+        {
+          level: "trace",
+          stream: process.stdout
+        },
+        {
+          level: "debug",
+          path: "log/" + strftime(DATE_FORMAT) + ".log"
+        },
+        {
+          level: "error",
+          path: "log/error/" + strftime(DATE_FORMAT) + ".log"
+        }
+      ]
+    });
+  }
+
+  return loggers[name];
+};
