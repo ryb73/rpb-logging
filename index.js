4851b3be8b1ddc782af77ae12e376a1ea3d60757
Ryan Biwer <rpbiwer@gmail.com>
2015-06-23 16:20:05 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var bunyan   = require("bunyan"),
+    strftime = require("strftime");
+
+var DATE_FORMAT = "%Y-%m-%d-%H-%M-%S";
+
+module.exports = bunyan.createLogger({
+  name: "index",
+  streams: [
+    {
+      level: "trace",
+      stream: process.stdout
+    },
+    {
+      level: "debug",
+      path: "log/" + strftime(DATE_FORMAT) + ".log"
+    },
+    {
+      level: "error",
+      path: "log/error/" + strftime(DATE_FORMAT) + ".log"
+    }
+  ]
+});
